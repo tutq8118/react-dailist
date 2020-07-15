4926851e39a9bdaa22f436bb2e577506176d6669
Tu Tran <tutq3@fo.com>
2020-07-15 17:04:07 +0700

    Update: localStorage save

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -3,8 +3,11 @@ import noItemSVG from './../no-item.svg';
 
 function TotoItem(props) {
   return (
-    <div className={props.isCompleted ? 'TodoItem TodoItem-completed' : 'TodoItem'} onClick={props.action}>
-      {props.label}
+    <div title={props.isCompleted ? 'Unfinish item' : 'Finish item'} className={props.isCompleted ? 'TodoItem TodoItem-completed' : 'TodoItem'} onClick={props.action}>
+      <span>{props.label}</span>
+      <button className="btn-close" title="Remove item" onClick={props.removeItem}>
+        ×
+      </button>
     </div>
   );
 }
@@ -15,12 +18,10 @@ export default class TodoApp extends Component {
     this.btnAddItem = React.createRef();
     this.inputAddItem = React.createRef();
 
+    const localItems = localStorage.getItem('todoItems') ? localStorage.getItem('todoItems') : [];
+
     this.state = {
-      items: [
-        { title: 'Coding', isCompleted: true },
-        { title: 'Reading book', isCompleted: false },
-        { title: 'Exercise', isCompleted: false },
-      ],
+      items: JSON.parse(localItems),
       openModal: false,
       inputWarning: false,
     };
@@ -38,12 +39,34 @@ export default class TodoApp extends Component {
     }
   };
 
+  handleRemoveItem = (item) => {
+    const { items } = this.state;
+    const index = items.indexOf(item);
+    
+    return (e) => {
+      e.stopPropagation();
+      console.log("removed");
+      this.setState({
+        items: [
+          ...items.slice(0, index),
+          ...items.slice(index + 1),
+        ],
+      });
+      const newItems = [
+        ...items.slice(0, index),
+        ...items.slice(index + 1),
+      ];
+      localStorage.setItem('todoItems', JSON.stringify(newItems));
+    }
+  }
+
   ToggleComplete = (item) => {
     const { items } = this.state;
     const isCompleted = item.isCompleted;
     const index = items.indexOf(item);
     
     return () => {
+      console.log("completed");
       this.setState({
         items: [
           ...items.slice(0, index),
@@ -54,6 +77,15 @@ export default class TodoApp extends Component {
           ...items.slice(index + 1),
         ]
       });
+      const newItems = [
+        ...items.slice(0, index),
+        {
+          ...item,
+          isCompleted: !isCompleted,
+        },
+        ...items.slice(index + 1),
+      ];
+      localStorage.setItem('todoItems', JSON.stringify(newItems));
     };
   };
 
@@ -72,6 +104,14 @@ export default class TodoApp extends Component {
           ],
           inputWarning: false,
         });
+        const newItems = [
+          ...items,
+          {
+            title: value,
+            isCompleted: false,
+          },
+        ];
+        localStorage.setItem('todoItems', JSON.stringify(newItems));
         this.inputAddItem.current.value = '';
       } else {
         this.setState({
@@ -101,6 +141,14 @@ export default class TodoApp extends Component {
         ],
         inputWarning: false,
       });
+      const newItems = [
+        ...items,
+        {
+          title: value,
+          isCompleted: false,
+        },
+      ];
+      localStorage.setItem('todoItems', JSON.stringify(newItems));
       this.inputAddItem.current.focus();
       this.inputAddItem.current.value = '';
     } else {
@@ -111,6 +159,8 @@ export default class TodoApp extends Component {
     }
   };
 
+  
+  
   render() {
     const { openModal, items, inputWarning } = this.state;
     return (
@@ -121,32 +171,40 @@ export default class TodoApp extends Component {
             <div className="TodoApp-List TodoApp-UpComming">
               <h3>Upcoming</h3>
               <div className="TodoApp-ListItems">
-                {items
-                  .filter((item) => item.isCompleted === false)
-                  .map((item, index) => {
-                    return <TotoItem action={this.ToggleComplete(item)} key={index} label={item.title} isCompleted={item.isCompleted}></TotoItem>;
-                  })}
+                {items.filter((item) => item.isCompleted === false).length > 0 &&
+                  items
+                    .filter((item) => item.isCompleted === false)
+                    .map((item, index) => {
+                      return <TotoItem action={this.ToggleComplete(item)} removeItem={this.handleRemoveItem(item)} key={index} label={item.title} isCompleted={item.isCompleted}></TotoItem>;
+                    })}
+                {items.filter((item) => item.isCompleted === false).length === 0 && <p className="TodoApp-noItem">No item...</p>}
               </div>
             </div>
             <div className="TodoApp-List TodoApp-Finished">
               <h3>Finished</h3>
               <div className="TodoApp-ListItem">
-                {items
-                  .filter((item) => item.isCompleted === true)
-                  .map((item, index) => {
-                    return <TotoItem action={this.ToggleComplete(item)} key={index} label={item.title} isCompleted={item.isCompleted} />;
-                  })}
+                {items.filter((item) => item.isCompleted === true).length > 0 &&
+                  items
+                    .filter((item) => item.isCompleted === true)
+                    .map((item, index) => {
+                      return <TotoItem action={this.ToggleComplete(item)} removeItem={this.handleRemoveItem(item)} key={index} label={item.title} isCompleted={item.isCompleted} />;
+                    })}
+                {items.filter((item) => item.isCompleted === true).length === 0 && <p className="TodoApp-noItem">No item...</p>}
               </div>
             </div>
           </main>
         )}
-        {items.length === 0 && <main className="noItem">
+        {items.length === 0 && (
+          <main className="noItem">
             <figure onClick={this.openModal}>
-              <img src={noItemSVG} alt=""/>
+              <img src={noItemSVG} alt="" />
             </figure>
-            <p>Seems like You have no list. <br/>
-            Just try to create some...</p>
-          </main>}
+            <p>
+              Seems like You have no list. <br />
+              Just try to create some...
+            </p>
+          </main>
+        )}
         <footer>
           <button className="TodoApp-AddBtn" onClick={this.openModal}>
             +
@@ -159,15 +217,12 @@ export default class TodoApp extends Component {
                 ×
               </button>
               <div className="Modal-content">
-                <input
-                  ref={this.inputAddItem}
-                  type="text"
-                  placeholder="Add a new item"
-                  onKeyDown={this.handleKeyDown}
-                  onBlur={this.handleBlur}
-                  autoFocus={true}
-                />
-                {inputWarning && <em className="form-error"><small>Please type something...</small></em>}
+                <input ref={this.inputAddItem} type="text" placeholder="Add a new item" onKeyDown={this.handleKeyDown} onBlur={this.handleBlur} autoFocus={true} />
+                {inputWarning && (
+                  <em className="form-error">
+                    <small>Please type something...</small>
+                  </em>
+                )}
               </div>
               <div className="Modal-footer">
                 <button ref={this.btnAddItem} onClick={this.handAddNewItem} className="btn btn-green">
@@ -181,13 +236,3 @@ export default class TodoApp extends Component {
     );
   }
 }
-
-// Requirements:
-// Viết một app todo-list có giao diện như hình https://cdn.glitch.com/780fd861-6c5c-464f-8b1b-c3c0ed64e30a%2FPasted_Image_4_20_20__9_05_PM.png?v=1587384320345
-// Không cần phải giống 100%, nhưng càng giống càng tốt và phải có tính thẩm mỹ
-// Illustration có thể tải ở đây https://undraw.co/illustrations hoặc các nguồn khác và upload lên Glitch assets
-// Cần làm:
-// - Màn hình danh sách todo list, nếu trống thì hiển thị một hình nào đó như trong ảnh
-// - Khi ấn nút + để tạo todo mới thì hiển thị modal có chứa 1 text input và nút để add
-// - Khi ấn vào 1 item thì sẽ toggle trạng thái isDone của nó
-// - Nếu isDone là true thì cho vào danh sách Finished, còn không thì ở Upcoming
